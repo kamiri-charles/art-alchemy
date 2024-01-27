1c3f762eb62e11a63dc63d0541e79f8067dfdc8c
Charles Kamiri <charleskamiri6@gmail.com>
2024-01-27 14:36:41 +0300

    feat: Add some header content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,20 @@ import './styles.scss';
 const Header:React.FC = () => {
   return (
     <div className='header'>
+      <div className="left">
 
+        <div className="menu-icon">
+          <i className="bx bx-menu"></i>
+        </div>
+
+        <div className="logo">
+          <span>Art</span>
+          <span>Alchemy</span>
+        </div>
+        
+      </div>
+
+      <div className="right"></div>
     </div>
   )
 }
